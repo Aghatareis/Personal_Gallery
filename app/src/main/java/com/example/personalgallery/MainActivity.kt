c83e6333977e433af285d1773240a3c52e8a7a97
PedroFrois29 <pedroka.frois@gmail.com>
2023-06-06 17:15:56 -0300

    pjplkoihu

--- app/src/main/java/com/example/personalgallery/MainActivity.kt
+++ app/src/main/java/com/example/personalgallery/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.personalgallery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.personalgallery.ui.theme.PersonalGalleryTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,26 +34,63 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ){}
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GaleriaMoldura(
+    IdImagem: Int,
+    IdTexto_1: Int,
+    IdTexto_2: Int
+) {
+    Card {
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = IdImagem),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(300.dp)
+            )
+
+            Text(
+                text = stringResource(id = IdTexto_1),
+                fontSize = 25.sp,
+                modifier = Modifier
+                    .padding(3.dp)
+            )
+
+            Text(
+                text = stringResource(id = IdTexto_2),
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun PreviewMoldura() {
     PersonalGalleryTheme {
-        Greeting("Android")
+        Column {
+            GaleriaMoldura(
+                IdImagem = R.drawable.imagem_1,
+                IdTexto_1 = R.string.fotografia_1,
+                IdTexto_2 = R.string.data_1
+            )
+
+            GaleriaMoldura(
+                IdImagem = R.drawable.imagem_2,
+                IdTexto_1 = R.string.fotografia_2,
+                IdTexto_2 = R.string.data_2
+            )
+        }
     }
 }
